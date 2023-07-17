13b5b3414c250799479dafc0fcd90b822b5cf2c7
Andrew-Forest1 <aforest41@yahoo.com>
2023-07-17 12:10:27 -0700

    Downloaded assets and created UI

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+	public int maxHealth = 100;
+	public int health = 100;
+	public Text healthText;
+	public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			if(health < maxHealth)
+			{
+				health += 10;
+			}
+		}
+
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			if (health > 0)
+			{
+				health -= 10;
+			}
+		}
+
+		healthSlider.value = (float) health / (float) maxHealth;
+		healthText.text = health.ToString() + " / " + maxHealth.ToString();
+    }
+
+
+}
